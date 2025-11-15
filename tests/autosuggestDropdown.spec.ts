ad99681d60689378fb038c65b045e04c2271fad4
hemnekapil <hemnekapil24@gmail.com>
2025-11-15 11:12:54 +0530

    autosuggestplaywright

--- tests/autosuggestDropdown.spec.ts
+++ tests/autosuggestDropdown.spec.ts
@@ -15,4 +15,18 @@ test("auto suggest dropdown with wait", async ({ page }) => {
   const options: Locator = page.locator("ul > li");
   //count options
   const count: number = await options.count();
+  console.log(`Number of suggested options: ${count}`);
+});
+//using for loop with nth()
+test("print auggestions with for loop", async ({ page }) => {
+  await page.goto("https://www.flipkart.com");
+  await page.locator('input[name="q"]').fill("smart");
+  await page.waitForTimeout(3000);
+  const options: Locator = page.locator("ul > li");
+  const count: number = await options.count();
+  //print each option
+  for (let i = 0; i < count; i++) {
+    const text = await options.nth(i).innerText();
+    console.log(text);
+  }
 });
